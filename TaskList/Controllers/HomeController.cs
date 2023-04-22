f7a213d2833ef55b53199b67850e2f437f3e7d21
yunusemrekosar <yunusemrekosar2@gmail.com>
2023-04-22 20:30:49 +0300

    accontcontroller

--- TaskList/Controllers/HomeController.cs
+++ TaskList/Controllers/HomeController.cs
@@ -9,20 +9,20 @@ namespace TaskList.Controllers
             return View();
         }
 
-        public IActionResult Register() 
-        {
-            return View();
-        }
+        //public IActionResult Register() 
+        //{
+        //    return View();
+        //}
 
-        public IActionResult Login()
-        {
-            return View();
-        }
+        //public IActionResult Login()
+        //{
+        //    return View();
+        //}
 
-        public IActionResult ForgotPassword()
-        {
-            return View();
-        }
+        //public IActionResult ForgotPassword()
+        //{
+        //    return View();
+        //}
 
         public IActionResult Users()
         {
