e3ed60c81a94b9b7b72bffaff6d0f69c29bb628b
Kerim Aykut Şengür <kaykut23@gmail.com>
2023-04-23 01:13:57 +0300

    createtask eklendi

---
+++ TaskList/Controllers/TaskController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskList.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult TaskDetail()
+        {
+            return View();
+        }
+
+        public IActionResult CreateTask()
+        {
+            return View();
+        }
+    }
+}
